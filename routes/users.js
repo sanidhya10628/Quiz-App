f31973b93d82556131a55392963057e57329c633
Sanidhya Mahajan <sanidhya10628@gmail.com>
2021-06-25 08:46:47 +0530

    added partials and error messages

--- routes/users.js
+++ routes/users.js
@@ -17,27 +17,37 @@ const isLogin = (req, res, next) => {
 }
 
 
-//login page
+// after successful Login
 router.get('/dashboard', isLogin, async (req, res) => {
 
     const currUser = await User.findById(req.session.user_id);
     res.render('dashboard', { currUser })
 })
+
+//login page
 router.get('/login', (req, res) => {
     res.render('login')
 })
 
 
 router.post('/login', async (req, res) => {
+    let errors = [];
     const { email, password } = req.body;
     const currUser = await User.findOne({ email });
-    const isValidPassword = await bcrypt.compare(password, currUser.password);
-    if (isValidPassword) {
-        req.session.user_id = currUser._id;
-        res.redirect('/dashboard')
+    if (currUser) {
+        const isValidPassword = await bcrypt.compare(password, currUser.password);
+        if (isValidPassword) {
+            req.session.user_id = currUser._id;
+            res.redirect('/dashboard')
+        }
+        else {
+            errors.push({ msg: 'Invalid Email or Password' })
+            res.render('login', { errors, email, password })
+        }
     }
     else {
-        res.redirect('/login')
+        errors.push({ msg: 'Invalid Email or Password' })
+        res.render('/login', { errors, email, password })
     }
 })
 
@@ -49,43 +59,34 @@ router.get('/register', (req, res) => {
 
 
 
-//
+// post register route
 
 router.post('/register', async (req, res) => {
     let errors = [];
     const { name, email, password, password2 } = req.body;
-    // User.findOne({ email: email }).then(user => {
-    //     if (user) {
-    //         // user exists
-    //         errors.push({ msg: 'Email already exists' })
-    //         res.render('register')
-    //     } else {
-    //         User.password = hashPassword(password);
-    //     }
-    // })
+    const isUser = await User.findOne({ email: email })
+    if (!isUser) {
+        const hash = await bcrypt.hash(password, 12);
+        const newUser = await new User({
+            name,
+            email,
+            password: hash
+        })
 
-    const hash = await bcrypt.hash(password, 12);
-    const newUser = await new User({
-        name,
-        email,
-        password: hash
-    })
-
-    await newUser.save();
-
-    res.redirect('/login');
+        await newUser.save();
+        res.redirect('/login');
+    }
+    else {
+        errors.push({ msg: 'Email already exists! Try Using another Email' })
+        res.render('register', { errors, name, email, password, password2 });
+    }
 })
 
-// const hashPassword = async (pw) => {
-//     const salt = await bcrypt.genSalt(12);
-//     const hash = await bcrypt.hash(pw, salt);
-//     console.log(salt);
-//     console.log(hash);
-// }
-
-
+// Log Out
 router.post('/logout', (req, res) => {
     req.session.user_id = null;
     res.redirect('/');
 })
+
+
 module.exports = router;
